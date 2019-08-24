de4ca536b9880c6742d71a43c264c6599bb4dcdf
Tesfaye Gari <TGari@chemtrec.com>
2019-08-24 11:10:58 -0400

    Fixed all the codes final

--- SPFxDemo/src/webparts/displayLists/DisplayListsWebPart.ts
+++ SPFxDemo/src/webparts/displayLists/DisplayListsWebPart.ts
@@ -4,24 +4,48 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-webpart-base';
+import { SPComponentLoader } from '@microsoft/sp-loader';
 require('./style.css');
 import * as strings from 'DisplayListsWebPartStrings';
+import { ILists, ISPList } from "./ISPLists";
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 export interface IDisplayListsWebPartProps {
   description: string;
-  dob: string;
 }
 
 export default class DisplayListsWebPart extends BaseClientSideWebPart<IDisplayListsWebPartProps> {
-  private calculateAge(dateOfBirth: string){
-    let dob = new Date(dateOfBirth);
-    return (new Date()).getFullYear() - dob.getFullYear();
-  }
+  
   public render(): void {
+    SPComponentLoader.loadCss('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css');
     this.domElement.innerHTML = `
-      <h1>${this.properties.description}'s Age :</h1>
-      <h2>${this.properties.description}, Your age is  ${this.calculateAge(this.properties.dob)} Years</h2>
+      
+      <h1>Lists under this ste</h1>
+      <ul id="listDisplay" class="list-group">
+        <li class="list-group-item">List 1 (2)</li>
+        <li class="list-group-item">List 2 (4)</li>
+        <li class="list-group-item">List 3 (112)</li>
+        <li class="list-group-item">List 4 (23)</li>
+      </ul>
       `;
+      this._getListData().then(response => {
+        console.log(response);
+        let myListsHtml = '';
+        for(let list of response.value){
+          console.log(list.Title);
+          myListsHtml += `<li class="list-group-item"> ${list.Title} (${list.ItemCount}) Items </li>`;
+        }
+        
+        // response.forEach(list => { myListsHtml += `<li> ${list.Title} (${list.ItemCount}) Items </li>`; });
+        this.domElement.querySelector('#listDisplay').innerHTML = myListsHtml;
+      }, error => {console.log('Error Happened', error);});
+  }
+
+  private _getListData(): Promise<ILists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
   }
 
   protected get dataVersion(): Version {
@@ -40,10 +64,7 @@ export default class DisplayListsWebPart extends BaseClientSideWebPart<IDisplayL
               groupName: strings.BasicGroupName,
               groupFields: [
                 PropertyPaneTextField('description', {
-                  label: 'Your Name'
-                }),
-                PropertyPaneTextField('dob', {
-                  label: 'Date of Birth'
+                  label: 'Webpart Title'
                 })
               ]
             }
---
+++ SPFxDemo/src/webparts/displayLists/ISPLists.ts
@@ -0,0 +1,9 @@
+export interface ISPList{
+  Title: string;
+  ItemCount: number;
+}
+
+export interface ILists{
+  value: ISPList[];
+}
+
